9dc4d10cc6cba3264af6105a5aec390ec7be7c03
Lyohha <325alex.000@gmail.com>
2019-11-20 20:16:00 +0200

    появились первые наброски главного меню

--- src/ua/lyohha/Main.java
+++ src/ua/lyohha/Main.java
@@ -11,8 +11,13 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("mainmenupage\\MainMenuPage.fxml"));
+        //scene.getStylesheets().add
+        // (HelloWorld.class.getResource("HelloWorld.css").toExternalForm());
+
         primaryStage.setTitle("AOS-C KhNTU Edition");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Scene scene = new Scene(root, 600, 480);
+        scene.getStylesheets().add(getClass().getResource("mainmenupage\\MainMenuPage.css").toExternalForm());
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 
