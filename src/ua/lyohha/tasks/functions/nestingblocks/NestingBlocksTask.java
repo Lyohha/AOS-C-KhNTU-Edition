62949a4c1ee50abfc4adb40dd0a5f1a8fc6ddee8
Lyohha <325alex.000@gmail.com>
2019-12-01 20:01:30 +0200

    начало: Классы памяти: вложенность блоков

---
+++ src/ua/lyohha/tasks/functions/nestingblocks/NestingBlocksTask.java
@@ -0,0 +1,35 @@
+package ua.lyohha.tasks.functions.nestingblocks;
+
+import com.sun.istack.internal.NotNull;
+import javafx.scene.layout.Pane;
+import ua.lyohha.tasks.Task;
+
+public class NestingBlocksTask implements Task {
+    private String taskName = "Классы памяти: вложенность блоков";
+    private TemplateGenerator templateGenerator;
+
+    @Override
+    public void create() {
+        templateGenerator = new TemplateGenerator();
+    }
+
+    @Override
+    public int getCountAnswers() {
+        return 4;
+    }
+
+    @Override
+    public String[] getAnswers() {
+        return templateGenerator.getAnswers();
+    }
+
+    @Override
+    public String getTaskName() {
+        return taskName;
+    }
+
+    @Override
+    public void setTaskCode(@NotNull Pane pane) {
+        templateGenerator.setTemplate(pane);
+    }
+}
---
+++ src/ua/lyohha/tasks/functions/nestingblocks/TemplateGenerator.java
@@ -0,0 +1,99 @@
+package ua.lyohha.tasks.functions.nestingblocks;
+
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import ua.lyohha.tasks.CodeGenerator;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class TemplateGenerator {
+    private List<HBox> lines = new ArrayList<>();
+    private String[] answers = new String[2];
+    private Random random;
+
+    public TemplateGenerator() {
+        createTemplate();
+    }
+
+    public void setTemplate(Pane pane) {
+        for (int i = 0; i < lines.size(); i++) pane.getChildren().add(lines.get(i));
+    }
+
+    public String[] getAnswers() {
+        return answers;
+    }
+
+    private void createTemplate() {
+        random = new Random(System.currentTimeMillis());
+        createFirstsLine();
+        createVariables();
+        createFirstExpression();
+        //createSecondExpression(text);
+        createLastLine();
+    }
+
+    private void createVariables() {
+
+    }
+
+    private void createFirstExpression() {
+
+    }
+
+    private void createSecondExpression() {
+
+    }
+
+    private void createThirdExpression()
+    {
+        List<Integer> l = new ArrayList<Integer>();
+
+    }
+
+    private List<Integer> getPosition()
+    {
+        List<Integer> pos = new ArrayList<>();
+
+        return pos;
+    }
+
+    private void createFirstsLine() {
+        lines.add(new HBox(
+                CodeGenerator.createPart("#include", CodeGenerator.CodeType.DIRECTIVE),
+                CodeGenerator.createPart(" <stdio.h>", CodeGenerator.CodeType.LIBRARY)
+        ));
+
+        lines.add(new HBox(
+                CodeGenerator.createPart("#define", CodeGenerator.CodeType.DIRECTIVE),
+                CodeGenerator.createPart(" PRINT(", CodeGenerator.CodeType.DEFINE1),
+                CodeGenerator.createPart("int", CodeGenerator.CodeType.OPERATOR),
+                CodeGenerator.createPart(")", CodeGenerator.CodeType.DEFINE1),
+                CodeGenerator.createPart(" printf("),
+                CodeGenerator.createPart("\"%d\\n\"", CodeGenerator.CodeType.STRING),
+                CodeGenerator.createPart(", "),
+                CodeGenerator.createPart("int", CodeGenerator.CodeType.OPERATOR),
+                CodeGenerator.createPart(")")
+        ));
+
+        lines.add(new HBox(
+                CodeGenerator.createPart(" ")
+        ));
+
+        lines.add(new HBox(
+                CodeGenerator.createPart("void", CodeGenerator.CodeType.OPERATOR),
+                CodeGenerator.createPart(" main()")
+        ));
+
+        lines.add(new HBox(
+                CodeGenerator.createPart("{")
+        ));
+    }
+
+    private void createLastLine() {
+        lines.add(new HBox(
+                CodeGenerator.createPart("}")
+        ));
+    }
+}
